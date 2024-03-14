356dad899a81853cccd2f6043818ba03914a23ca
rtarun9 <rtarun0913@gmail.com>
2024-03-14 07:37:08 +0530

    Code and comment cleanup

---
+++ include/math_utils.h
@@ -0,0 +1,12 @@
+#ifndef __MATH_UTILS_H__
+#define __MATH_UTILS_H__
+
+#include "type.h"
+
+// Utility function to convert degree to radians.
+f32 degree_to_radians(const f32 degree)
+{
+    return degree * (3.14159 / 180.0f);
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/math_utils.h"
 #include "../include/type.h"
 
 // Dimensions of the OVERALL framebuffer.
 // Note that this includes dimension for both game map and the 3D image (which will be on the right side of game map).
-#define FB_NUMBER_OF_COLUMNS 512 * 2
+#define FB_NUMBER_OF_COLUMNS 1024
 #define FB_NUMBER_OF_ROWS 512
 
 // Dimensions for each IMAGE the framebuffer constitutes.
 // This includes a game map on the left, and 3D image on the right.
-#define NUMBER_OF_COLUMNS 512
-#define NUMBER_OF_ROWS FB_NUMBER_OF_ROWS
+#define IMAGE_NUMBER_OF_COLUMNS (FB_NUMBER_OF_COLUMNS / 2)
+#define IMAGE_NUMBER_OF_ROWS FB_NUMBER_OF_ROWS
 
 // Max pixel value (required for PPM format).
 #define MAXIMUM_PIXEL_VALUE 255
@@ -22,21 +23,15 @@
 #define MAP_NUMBER_OF_ROWS 16
 
 // With respect to pixels of image and map grid cells, how many pixels one map entry will represent.
-const u8 MAP_CELL_ENTRY_WIDTH = NUMBER_OF_COLUMNS / MAP_NUMBER_OF_COLUMNS;
-const u8 MAP_CELL_ENTRY_HEIGHT = NUMBER_OF_ROWS / MAP_NUMBER_OF_ROWS;
-
-// Utility function to convert degree to radians.
-f32 degree_to_radians(const f32 degree)
-{
-    return degree * (3.14159 / 180.0f);
-}
+#define MAP_CELL_ENTRY_WIDTH (u16)(IMAGE_NUMBER_OF_COLUMNS / MAP_NUMBER_OF_COLUMNS)
+#define MAP_CELL_ENTRY_HEIGHT (u16)(IMAGE_NUMBER_OF_ROWS / MAP_NUMBER_OF_ROWS)
 
 // Write contents of framebuffer to a PPM file.
 // Returns BOOL_FALSE if function failed to write to PPM file.
 bool write_to_file(const u32 *frame_buffer)
 {
     FILE *file = NULL;
-    errno_t err = fopen_s(&file, "output.ppm", "w+");
+    const errno_t err = fopen_s(&file, "output.ppm", "w+");
 
     if (err != 0)
     {
@@ -57,12 +52,13 @@ bool write_to_file(const u32 *frame_buffer)
     {
         for (u16 j = 0; j < FB_NUMBER_OF_COLUMNS; j++)
         {
-            u32 current_pixel_value = frame_buffer[i * FB_NUMBER_OF_COLUMNS + j];
+            const u32 current_pixel_value = frame_buffer[i * FB_NUMBER_OF_COLUMNS + j];
             fprintf(file, "%u %u %u ", (current_pixel_value >> 16) & 0xff, (current_pixel_value >> 8) & 0xff,
                     current_pixel_value & 0xff);
         }
         fprintf(file, "\n");
     }
+
     fclose(file);
 
     return BOOL_TRUE;
@@ -79,9 +75,9 @@ void draw_rectangle(u32 *frame_buffer, const u32 color, const u16 x, const u16 y
     {
         for (u16 v = 0; v < rect_width; v++)
         {
-            // Note the horizontal check is done against NUMBER_OF_COLUMNS (so we only draw on left half of FB),
+            // Note the horizontal check is done against IMAGE_NUMBER_OF_COLUMNS (so we only draw on left half of FB),
             // But when writing to FB we use FB_NUMBER_OF_COLUMNS to index to FB (to prevent writing to right 3D image).
-            if (x + v < NUMBER_OF_COLUMNS && y + k < NUMBER_OF_ROWS)
+            if (x + v < IMAGE_NUMBER_OF_COLUMNS && y + k < IMAGE_NUMBER_OF_ROWS)
             {
                 frame_buffer[(y + k) * FB_NUMBER_OF_COLUMNS + v + x] = color;
             }
@@ -91,11 +87,24 @@ void draw_rectangle(u32 *frame_buffer, const u32 color, const u16 x, const u16 y
 
 int main()
 {
+    printf("Raycaster Config\nimage_number_of_rows :: %u, image_number_of_columns :: %u\nfb_number_of_rows :: %u, "
+           "fb_number_of_columns :: %u\nmap_number_of_rows :: %u, map_number_of_columns :: %u\n",
+           IMAGE_NUMBER_OF_ROWS, IMAGE_NUMBER_OF_COLUMNS, FB_NUMBER_OF_ROWS, FB_NUMBER_OF_COLUMNS, MAP_NUMBER_OF_ROWS,
+           MAP_NUMBER_OF_COLUMNS);
+
     // Values of player position x and y.
-    // These values are tied to map coordinates, but are floating point.
-    // I.E the range of values for player x and y is (f32)0.0, (f32)map_number_of_rows - 1 and so on.
-    const f32 player_x = 2.0f;
-    const f32 player_y = 2.0f;
+    // These values are tied to map coordinates.
+    // I.E the range of values for player x and y is 0, map_number_of_rows - 1 and so on.
+    const u16 player_x = 4u;
+    const u16 player_y = 2u;
+
+    // player_angle is the angle (with respect to the x axis) that the player is looking at.
+    const f32 player_angle = degree_to_radians(90.0f);
+
+    // FOV (Field of view)
+    // Let the FOV be theta.
+    // Then, we consider that the player can see from -0.5f * theta + player_angle, 0.5f * theta + player_angle.
+    const f32 player_fov = degree_to_radians(45.0f);
 
     // Rules for the hardcoded game map. Map width and height must be divisible by NUMBER_OF_COLUMNS and NUMBER_OF_ROWS.
     // In this map (1D char array), a 0 is a walled zone. Empty entries are non-walls. 0 will imply that visually a
@@ -124,12 +133,9 @@ int main()
     u32 *frame_buffer = (u32 *)malloc(sizeof(u32) * FB_NUMBER_OF_COLUMNS * FB_NUMBER_OF_ROWS);
 
     // Set a background color for entire FB.
-    for (u16 i = 0; i < FB_NUMBER_OF_ROWS; i++)
+    for (u32 i = 0; i < FB_NUMBER_OF_ROWS * FB_NUMBER_OF_COLUMNS; i++)
     {
-        for (u16 j = 0; j < FB_NUMBER_OF_COLUMNS; j++)
-        {
-            frame_buffer[i * FB_NUMBER_OF_COLUMNS + j] = 0x000099cc;
-        }
+        frame_buffer[i] = 0x000099cc;
     }
 
     // Create the game map.
@@ -139,10 +145,6 @@ int main()
         {
             // If entry [i][j] of game_map is a 0, write black from i * MAP_CELL_ENTRY_WIDTH to i * MAP_CELL_ENTRY_WIDTH
             // + 1 (and similarly for the height).
-            // Now, say in the MAP, (10, 10) is marked with a 0.
-            // How will this be mapped onto the framebuffer?
-            // The STARTING (i.e top left) pixel of rectangle will be 10 * MAP_CELL_ENTRY_WIDTH and similarly for the
-            // other axis.
             if (game_map[i * MAP_NUMBER_OF_COLUMNS + j] == '0')
             {
                 draw_rectangle(frame_buffer, 0u, j * MAP_CELL_ENTRY_WIDTH, i * MAP_CELL_ENTRY_HEIGHT,
@@ -150,12 +152,6 @@ int main()
             }
         }
     }
-
-    // Draw rectangle for player on the image (not the MAP).
-    const u16 image_player_x = (u16)((NUMBER_OF_COLUMNS * player_x) / (f32)MAP_NUMBER_OF_COLUMNS);
-    const u16 image_player_y = (u16)((NUMBER_OF_ROWS * player_y) / (f32)MAP_NUMBER_OF_ROWS);
-
-    draw_rectangle(frame_buffer, 0xffffffff, image_player_x, image_player_y, 4u, 4u);
 
     // player_a : The angle the player is looking at, with respect to the x axis.
     // Consider the following triangle.
@@ -174,60 +170,62 @@ int main()
     // From player_position, with hypotenuse length as c, the vector that goes from position to look at direction is:
     // (player_x + c * cos_a, player_y + c * sin_a)
 
-    // Draw a line from player position to where it is looking.
-    // 2PI radian = 360 degree
-    // so, X degree in radian is x * (PI / 180)
-    const f32 player_angle = degree_to_radians(80.0f);
-
-    // FOV (Field of view)
-    // Let the FOV be theta.
-    // Then, we consider that the player can see from -0.5f * theta + player_angle, 0.5f * theta + player_angle.
-    const f32 player_fov = degree_to_radians(45.0f);
-
     // We will be shooting 512 rays. This is because when drawing the 3D image, the image width will be 512 pixels.
     // In the 3D image, each pixel (horizontally) represents one of these casted rays (which is why we need to cast only
     // 512 rays).
 
-    // Process to create 3D image.
-    // Start with the center (vertically) of 3D image.
-    // Each of 512 casted rays represent one pixel of 3D image horizontally.
-    // When a obstacle has been hit, find the distance to player, find inverse, and move (inverse / 2) pixels up and
-    // down from the center of 3D image. The color is the same as the obstacle color.
-    f32 angle_increment_to_cast_512_rays = player_fov / 512.0f;
+    const f32 angle_increment_to_cast_512_rays = player_fov / 512.0f;
     u16 ray_number = 0u;
+
+    // player coordinates that are tied to image (player_x/y is tied to the map).
+    const u16 image_player_x = (player_x * IMAGE_NUMBER_OF_COLUMNS / MAP_NUMBER_OF_COLUMNS);
+    const u16 image_player_y = (player_y * IMAGE_NUMBER_OF_ROWS / MAP_NUMBER_OF_ROWS);
 
     for (f32 angle = -0.5f * player_fov; angle <= 0.5f * player_fov; angle += angle_increment_to_cast_512_rays)
     {
         u16 distance = 0xffff;
-        for (u16 c = 4u; c < 500u; c += 5)
+        for (u16 c = 1u; c < IMAGE_NUMBER_OF_ROWS; c += 3)
         {
             const u16 look_at_x = (u16)(cos(player_angle + angle) * c + image_player_x);
             const u16 look_at_y = (u16)(sin(player_angle + angle) * c + image_player_y);
 
             // If the 'ray' hits a wall, break out of the loop.
             // For that, conversion from fb coordinate to map coordinate is required.
-            const u16 map_look_at_x = (MAP_NUMBER_OF_COLUMNS * look_at_x / NUMBER_OF_COLUMNS);
-            const u16 map_look_at_y = (MAP_NUMBER_OF_ROWS * look_at_y / NUMBER_OF_ROWS);
+            const u16 map_look_at_x = (MAP_NUMBER_OF_COLUMNS * look_at_x / IMAGE_NUMBER_OF_COLUMNS);
+            const u16 map_look_at_y = (MAP_NUMBER_OF_ROWS * look_at_y / IMAGE_NUMBER_OF_ROWS);
 
             if (map_look_at_x < MAP_NUMBER_OF_COLUMNS && map_look_at_y < MAP_NUMBER_OF_ROWS &&
                 game_map[map_look_at_x + map_look_at_y * MAP_NUMBER_OF_COLUMNS] == '0')
             {
-                distance = NUMBER_OF_ROWS - c;
+                distance = (u16)(IMAGE_NUMBER_OF_ROWS - c);
                 break;
             }
 
-            // for debugging the ray cast.
-            draw_rectangle(frame_buffer, 0x00ff0fff, look_at_x, look_at_y, 4u, 4u);
+            // for debugging the ray cast (in dotted fashion).
+            if (c & 1)
+            {
+                draw_rectangle(frame_buffer, 0x00ff0fff, look_at_x, look_at_y, 4u, 4u);
+            }
         }
 
+        printf("dist :: %u\n", distance);
+        // Process to create 3D image.
+        // Start with the center (vertically) of 3D image.
+        // Each of 512 casted rays represent one pixel of 3D image horizontally.
+        // When a obstacle has been hit, find the distance to player, find inverse, and move (inverse / 2) pixels up and
+        // down from the center of 3D image. The color is the same as the obstacle color.
         for (i16 h = -distance / 2; h < distance / 2; h += 1)
         {
-            frame_buffer[((NUMBER_OF_ROWS / 2) + h) * FB_NUMBER_OF_COLUMNS + ray_number + NUMBER_OF_COLUMNS] =
-                0x00aabbcc;
+            frame_buffer[((IMAGE_NUMBER_OF_ROWS / 2) + h) * FB_NUMBER_OF_COLUMNS + ray_number +
+                         IMAGE_NUMBER_OF_COLUMNS] = 0x00aabbcc;
         }
 
         ray_number++;
     }
+
+    // Draw rectangle for player on the image (not the MAP).
+    // This is done at the last so debug raycast do not cover player viz.
+    draw_rectangle(frame_buffer, 0xffffffff, player_x * MAP_CELL_ENTRY_WIDTH, player_y * MAP_CELL_ENTRY_HEIGHT, 4u, 4u);
 
     if (write_to_file(frame_buffer) == BOOL_FALSE)
     {
